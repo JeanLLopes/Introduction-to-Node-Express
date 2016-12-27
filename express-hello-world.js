9244b5045da33f7b55a142389970dec7af2f58b1
JeanLLopes <jean.llopes@outlook.com>
2016-12-27 00:24:24 -0200

    Add Variables Enviable

--- express-hello-world.js
+++ express-hello-world.js
@@ -1,6 +1,12 @@
+//PARA RODAR O PROJETO USE O COMANDO
+//node express-hello-world.js 
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+//BUSCA O ARQUIVO .env PARA BUSCAR A PORTA, OU PEGA A PORTA PADRAO
+//QUE É A 3000 COMO INFORMADO ABAIXO
 const port = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
